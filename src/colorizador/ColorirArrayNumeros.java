e3a1de440ee784b8f00eb056a92fbb512d417a06
CThurler <c.thurlerr@gmail.com>
2025-04-10 23:10:42 -0300

    Add ColorirArrayNumeros

--- src/colorizador/ColorirArrayNumeros.java
+++ src/colorizador/ColorirArrayNumeros.java
@@ -1,5 +1,31 @@
 package colorizador;
 
+import java.util.Arrays;
+
 class ColorirArrayNumeros {
 
+		public static String texto(int array[], boolean cor){
+			if (cor == false) {
+				String texto = "\u001B[34m";
+				Arrays.sort(array);
+				for (int i : array) {
+					texto += " " + i;
+				}
+				return(texto);
+			} else {
+				String negativos = "\u001B[31m";
+				String zero = "\u001B[34m";
+				String positivos = "\u001B[32m";
+				
+				Arrays.sort(array);
+				
+				for (int i : array) {
+					if(i==0) zero += " " + i;
+					if(i<0) negativos += " " + i;
+					if(i>0) positivos += " " + i;
+				}
+				
+				return (negativos + zero + positivos);
+			}
+		}
 }
--- src/colorizador/Colorizador.java
+++ src/colorizador/Colorizador.java
@@ -1,6 +1,7 @@
 package colorizador;
 
 public class Colorizador {
+	private static int arrayNumeros[] = {1,2,3,4,5,2,3,4,0,-1,-2,-3,4,-4,2,87,-7,-66,-44};
 
 	public static void colorirTexto(String texto, String cor){
 		String text = ColorirTexto.texto(texto,cor);
@@ -10,9 +11,16 @@ public class Colorizador {
 		String text = ColorirTexto.fundo(texto,cor);
 		System.out.println(text);
 	}
+	public static void colorirArrayNumeros(int array[], boolean cor){
+		String text = ColorirArrayNumeros.texto(array,cor);
+		System.out.println(text);
+	}
 	
 	public static void main(String[] args) {
 		colorirTexto("teste","#FF0000");
+		colorirFundo("teste","#FF0000");
+		colorirArrayNumeros(arrayNumeros, true);
+		colorirArrayNumeros(arrayNumeros, false);
 	}
 	
 	
